4f509aca77cf33e2d7afa53bd3ba58abbcd6468a
HARSH PRANJAL <harshprnajal17030@gmail.com>
2024-05-30 23:19:30 +0530

    credentials update

--- API_1/config/db.py
+++ API_1/config/db.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 
-client = MongoClient("mongodb+srv://api1:hp4348.@cluster0.glypb4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("mongodb+srv://<username>:<password>.@cluster0.glypb4r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 db = client.todo_db
 
